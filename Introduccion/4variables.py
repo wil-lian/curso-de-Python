61ed0b5e4deb768f43cf4591633039ea39e1b0be
will <guillermototi11@gmail.com>
2023-04-20 09:51:01 -0400

    variables

--- Introduccion/4variables.py
+++ Introduccion/4variables.py
@@ -8,3 +8,30 @@ print(y)
 z = 8
 z = "william"
 print(z)
+
+#variables de salida
+x = "william"
+y = "fabricio"
+z = "tito"
+print(x,y,z)
+
+# generar multiples variables de salida
+a = "Python"
+b = "is"
+c = "awesome"
+print(a + b + c)
+
+# variable de salida como operador matematico
+f = 5
+g = 4
+print(5+4)
+
+# conbinacion con un numero con una cadena sale error
+t = 5
+u = "hola willilam"
+print(t + u)
+
+# lo mejor es generar multiples variables con , coma
+q = 5
+w = "hola fabricio"
+print(q, w)
